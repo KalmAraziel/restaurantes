88c6dc277dfa3f5fcb982cd2dbabd25b4989a001
Marcos Ahumada <marcos.ahumada@example.com>
2019-11-29 15:00:51 -0300

    inicio proyecto restaurants react

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import  Icon from 'react-native-vector-icons/FontAwesome'; 
+export default function App() {
+  
+  return (
+    <View style={styles.container}>
+    <Text>5 Tenedores!</Text>
+    <Button icon = {
+      <Icon 
+        type="material-community" name="arrow-right" size={15} color="white" />      
+      } 
+      title=" Boton !"></Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
