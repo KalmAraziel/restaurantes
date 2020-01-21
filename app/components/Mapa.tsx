a5602d4aff205e96a380b7cbc93119550066d039
Marcos Ahumada <marcos.ahumada@example.com>
2020-01-21 16:52:30 -0300

    mapa

--- app/components/Mapa.tsx
+++ app/components/Mapa.tsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 import { StyleSheet, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-
+import openMap from 'react-native-open-maps';
 export default function Mapa(props) {
     const {location, name } = props;
+    
+    const abrirMaps = () => {
+        openMap({ 
+            latitude: location.latitude, 
+            longitude: location.longitude,
+            zoom: 19,
+            query: name
+        });
+    }
+
+    
     return(
         <MapView
             style={styles.mapStyle}
             initialRegion={location}
-            onPress={()=> console.log("abriendo maps")}
+            onPress={openMap}
         >
             <Marker 
                 coordinate = {{
